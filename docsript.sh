646f34de307c5035d9d7a65f4b5c7a5cb493e450
gregoryit <mike25012000@gmail.com>
2022-09-17 22:32:21 +0000

    upd

---
+++ docsript.sh
@@ -0,0 +1,4 @@
+docker stop db library_app
+docker rm db library_app
+docker rmi library_app-app
+docker compose up
--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
-print('hello, library!')
+import mysql.connector
+import os
+from time import sleep
+
+# while True:
+#     try:
+#         mydb = mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password=os.environ['MYSQL_PASSWORD'],
+#         database='db'
+#         )
+#         break
+#     except:
+
+#         print('Connection failed. Try again..')
+#         sleep(1)
+
+# cursor = mydb.cursor()
+
+# while True:
+#     cursor.execute('select * from books')
+#     res = cursor.fetchall()
+#     print(res)
+#     input('press enter ')
