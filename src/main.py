7a8e3237ca9e30b92f0f5192c5a59556635e640f
gregoryit <mike25012000@gmail.com>
2022-09-18 00:01:13 +0000

    upd

--- src/main.py
+++ src/main.py
@@ -1,25 +1,37 @@
+# -*- coding: utf-8 -*-
 import mysql.connector
 import os
 from time import sleep
 
-# while True:
-#     try:
-#         mydb = mysql.connector.connect(
-#         host="localhost",
-#         user="root",
-#         password=os.environ['MYSQL_PASSWORD'],
-#         database='db'
-#         )
-#         break
-#     except:
+while True:
+    try:
+        mydb = mysql.connector.connect(
+        host="db",
+        user="root",
+        password=os.environ['MYSQL_PASSWORD'],
+        database='db'
+        )
+        break
+    except:
 
-#         print('Connection failed. Try again..')
-#         sleep(1)
+        print('Connection failed. Try again..')
+        sleep(1)
 
-# cursor = mydb.cursor()
+cursor = mydb.cursor()
+try:
+    cursor.execute("""CREATE TABLE books (
+                    idbooks INT NOT NULL AUTO_INCREMENT,
+                    title VARCHAR(45) NULL,
+                    PRIMARY KEY (idbooks));
+                    """)
+except:
+    print('error')
 
-# while True:
-#     cursor.execute('select * from books')
-#     res = cursor.fetchall()
-#     print(res)
-#     input('press enter ')
+while True:
+    book = input('Какую книгу вы хотите добавить? ')
+    cursor.execute(f""" INSERT INTO books (title) VALUES ('{book}');
+                """)
+    mydb.commit()
+    cursor.execute('select * from books')
+    res = cursor.fetchall()
+    print(res)
