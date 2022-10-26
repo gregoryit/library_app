d1b2d3129c518d43fa1764b7cf7168bde8e1ecf7
gregoryit <mike25012000@gmail.com>
2022-10-26 21:21:51 +0000

    upd

--- auto_test.py
+++ auto_test.py
@@ -1,8 +1,8 @@
 import os
 import json
 
-a = os.system('pycodestyle ./src > /dev/null 2>&1')
-os.system('bandit -r ./src -f json -o out.json > /dev/null 2>&1')
+a = os.system('python3 -m pycodestyle ./src > /dev/null 2>&1')
+os.system('python3 -m bandit -r ./src -f json -o ./out.json > /dev/null 2>&1')
 
 with open('out.json', 'r') as f:
     b = json.load(f)
