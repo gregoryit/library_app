f7af3f0df3b3d460577fc89dfc65a31f2067d5eb
gregoryit <mike25012000@gmail.com>
2022-12-21 14:59:55 +0000

    upd

--- auto_test.py
+++ auto_test.py
@@ -2,12 +2,11 @@ import os
 import json
 
 
-a = os.system('python3 -m pycodestyle ./src > /dev/null 2>&1')
+w = os.system('python3 -m pycodestyle ./src > /dev/null 2>&1')
 s = 'python3 -m bandit -r ./src/main.py -f json -o ./out.json > /dev/null 2>&1'
 os.system(s)
-
 with open('out.json', 'r') as f:
     b = json.load(f)
 
-if b['results'] or a:
+if b['results'] or w:
     raise RuntimeError('ошибочка')
