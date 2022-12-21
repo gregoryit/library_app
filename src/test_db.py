e7709804904f5b4941413d7f4c6d230ed3f6b0a2
gregoryit <mike25012000@gmail.com>
2022-12-21 20:37:44 +0000

    v6

--- src/test_db.py
+++ src/test_db.py
@@ -12,15 +12,13 @@ load_dotenv()
 MYSQL_USER = "root"
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
 MYSQL_DB = "testdb"
-MYSQL_HOST = "localhost"
+MYSQL_HOST = "db"
 MYSQL_PORT = 3306
 
 
 class MockDB(TestCase):
     @classmethod
     def setUpClass(cls):
-        w = os.system('docker ps -a > /dev/null 2>&1')
-        print(w)
         cls.cnx = mysql.connector.connect(
             host=MYSQL_HOST,
             user=MYSQL_USER,
